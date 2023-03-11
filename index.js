8200a20a4d5df45567393f7d464839c9e69261b3
Calvin Janitra <calvinjanitra9@gmail.com>
2023-03-11 21:01:37 +0700

    add slider to branch

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-let signup = document.querySelector('.signup')
+const wrapper = document.querySelector('.wrapper')
+const login = document.querySelector('.link-login')
+const regist = document.querySelector('.link-regist')
+
+regist.addEventListener('click', () => {
+    wrapper.classList.add('active')
+})
+
+login.addEventListener('click', () => {
+    wrapper.classList.remove('active')
+})
