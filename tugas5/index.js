baa2931e9f41d4fd45a815f77d83cd17baff00a0
Calvin Janitra <calvinjanitra9@gmail.com>
2023-03-25 23:29:10 +0700

    add file

---
+++ tugas5/index.js
@@ -0,0 +1,29 @@
+$(function () {
+    $(function () {
+        $('form').validate()
+    })
+})
+
+$( "form" ).on( "submit", function(e) {
+    var dataString = $(this).serialize();
+    
+    $.ajax({
+      type: "POST",
+      url: "bin/process.php",
+      data: dataString,
+      success: function () {
+        $("#contact_form").html("<div id='message'></div>");
+        $("#message")
+          .html("<h2>Contact Form Submitted!</h2>")
+          .append("<p>We will be in touch soon.</p>")
+          .hide()
+          .fadeIn(1500, function () {
+            $("#message").append(
+              "<img id='checkmark' src='images/check.png' />"
+            );
+          });
+      }
+    });
+    e.preventDefault();
+  });
+});
